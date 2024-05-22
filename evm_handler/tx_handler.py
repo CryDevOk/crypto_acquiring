6f250ed7a3c4e8774a2287722526ccfece18840d
test <johndoe@example.com>
2024-05-22 13:27:19 +0700

    feat: tx_conductor_coin tx_conductor_native

--- evm_handler/tx_handler.py
+++ evm_handler/tx_handler.py
@@ -5,27 +5,92 @@ from misc import get_logger, SharedVariables, amount_to_quote_amount, \
     get_round_for_rate, amount_to_display
 import asyncio
 import logging
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Any
 from decimal import Decimal
 import eth_utils
 import eth_abi
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from datetime import datetime, timedelta, timezone
 
 from web3_client import async_client, utils as web3_utils
-from db.database import DB, write_async_session, read_async_session
+from db.database import DB, write_async_session, read_async_session, UserAddress
 from db.models import Deposits, Coins
 from config import Config as Cfg, StatCode as St
 import api
 
 
-async def trx_balance(conn_creds, addr_id, address):
+async def coin_transfer_to_admin(conn_creds,
+                                 contract_address,
+                                 user_public,
+                                 user_private,
+                                 admin_public,
+                                 approve_id,
+                                 approve_public,
+                                 approve_private,
+                                 deposit_id,
+                                 amount,
+                                 tx_handler_period) -> tuple[None, Exception, tuple[Any, Any, Any], Any] | tuple[
+    Any, None, tuple[Any, Any, Any], Any]:
+    amount = int(amount)
+    async with async_client.AsyncEth(*conn_creds) as client:
+        contract = async_client.ERC20(client, contract_address, abi_info=web3_utils.erc20_abi)
+        try:
+            allowance = await contract.allowance(user_public, approve_public)
+        except Exception as exc:
+            return None, exc, (deposit_id, tx_handler_period, approve_id), conn_creds
+        else:
+            if allowance < amount:
+                try:
+                    await variables.gas_price_event.wait()
+                    await client.send_ether(user_public,
+                                            30_000_000,
+                                            approve_private,
+                                            gas_price=variables.gas_price,
+                                            gas=21000)
+
+                except Exception as exc:
+                    return None, exc, (deposit_id, tx_handler_period, approve_id), conn_creds
+                else:
+                    try:
+                        await variables.gas_price_event.wait()
+                        await contract.approve(approve_public, 9_999_999_999_999_999, user_private,
+                                               gas_price=variables.gas_price)
+                    except Exception as exc:
+                        return None, exc, (deposit_id, tx_handler_period, approve_id), conn_creds
+                    else:
+                        try:
+                            await variables.gas_price_event.wait()
+                            res = await contract.transfer_from(user_public,
+                                                               admin_public,
+                                                               amount,
+                                                               approve_private,
+                                                               gas_price=variables.gas_price)
+                        except Exception as exc:
+                            return None, exc, (deposit_id, tx_handler_period, approve_id), conn_creds
+                        else:
+                            return res, None, (deposit_id, tx_handler_period, approve_id), conn_creds
+            else:
+                try:
+                    await variables.gas_price_event.wait()
+                    res = await contract.transfer_from(user_public,
+                                                       admin_public,
+                                                       amount,
+                                                       approve_private,
+                                                       gas_price=variables.gas_price)
+                except Exception as exc:
+                    return None, exc, (deposit_id, tx_handler_period, approve_id), conn_creds
+                else:
+                    return res, None, (deposit_id, tx_handler_period, approve_id), conn_creds
+
+
+async def native_balance(conn_creds, addr_id, address):
     try:
         async with async_client.AsyncEth(*conn_creds) as client:
             res: int = await client.get_account_balance(address)
     except Exception as exc:
-        return None, exc, (addr_id, conn_creds)
+        return None, exc, (addr_id, address, conn_creds)
     else:
-        return res, None, (addr_id, conn_creds)
+        return res, None, (addr_id, address, conn_creds)
 
 
 async def trc20_balance(conn_creds, addr_id, contract_address, address: str):
@@ -48,16 +113,16 @@ async def admin_approve_native_bal(logger):
         if users:
             for addr_id, address in users:
                 conn_creds: List[Tuple[str, str]] = await variables.api_keys_pool.get()
-                reqs.append(asyncio.create_task(trx_balance(conn_creds, addr_id, address)))
+                reqs.append(asyncio.create_task(native_balance(conn_creds, addr_id, address)))
 
             results = await asyncio.gather(*reqs)
 
             for balance, err, req_ident in results:
-                addr_id, conn_creds = req_ident
+                addr_id, address, conn_creds = req_ident
                 await variables.api_keys_pool.put(conn_creds)
                 if not err:
-                    if balance <= variables.estimated_native_fee * Cfg.native_warning_threshold:
-                        logger.warning(f"{addr_id}: has balance {amount_to_display(balance, 6, Decimal('0.01'))} "
+                    if balance <= (variables.gas_price * 100000) * Cfg.native_warning_threshold:
+                        logger.warning(f"{addr_id} {address}: has balance {amount_to_display(balance, 18, Decimal('0.00001'))} "
                                        f"and can handle less then {Cfg.native_warning_threshold} transactions")
                     await db.upsert_balance(addr_id, St.native.v, balance, commit=True)
                 else:
@@ -137,6 +202,21 @@ async def update_coin_rates(logger: logging.Logger):
             else:
                 if coin[Coins.current_rate.key] != 1:
                     await db.update_coin(coin[Coins.contract_address.key], {Coins.current_rate.key: 1}, commit=True)
+
+
+async def update_gas_price(logger: logging.Logger):
+    conn_creds: List[Tuple[str, str]] = await variables.api_keys_pool.get()
+    try:
+        async with async_client.AsyncEth(*conn_creds) as client:
+            gas_price = int((await client.gas_price()) * 1.5)
+    except Exception as exc:
+        logger.error(exc)
+    else:
+        variables.gas_price_event.clear()
+        variables.gas_price = gas_price
+    finally:
+        variables.gas_price_event.set()
+        await variables.api_keys_pool.put(conn_creds)
 
 
 async def update_in_memory_accounts(logger: logging.Logger):
@@ -278,9 +358,96 @@ async def block_parser(conn_creds_1, conn_creds_2, logger: logging.Logger):
                         logger.info(f"handled block number {current_block}")
 
 
+async def tx_conductor_native(logger: logging.Logger):
+    reqs = []
+    async with write_async_session() as session:
+        db = DB(session, logger)
+        deposits = await db.get_and_lock_pending_deposits_native(7)
+
+        if deposits:
+            for deposit in deposits:
+                conn_creds: List[Tuple[str, str]] = await variables.api_keys_pool.get()
+                reqs.append(asyncio.create_task(native_transfer_to_admin(conn_creds=conn_creds, **deposit)))
+
+            results = await asyncio.gather(*reqs)
+
+            for tx_hash, err, req_ident, conn_creds in results:
+                await variables.api_keys_pool.put(conn_creds)
+                deposit_id, tx_handler_period = req_ident
+                if not err:
+                    await db.update_deposit_by_id(deposit_id, {Deposits.tx_hash_out.key: tx_hash,
+                                                               Deposits.locked_by_tx_handler.key: False}, commit=True)
+                elif tx_hash:
+                    logger.critical(f"{err} deposit_id: {deposit_id}, {tx_hash}")
+                    await db.update_deposit_by_id(deposit_id, {Deposits.tx_hash_out.key: tx_hash}, commit=True)
+                else:
+                    logger.error(f"{err} deposit_id: {deposit_id}")
+                    time_to_tx_handler = datetime.now(timezone.utc) + timedelta(seconds=tx_handler_period)
+                    tx_handler_period += 30
+                    await db.update_deposit_by_id(deposit_id, {Deposits.locked_by_tx_handler.key: False,
+                                                               Deposits.time_to_tx_handler.key: time_to_tx_handler,
+                                                               Deposits.tx_handler_period.key: tx_handler_period},
+                                                  commit=True)
+
+
+async def native_transfer_to_admin(conn_creds,
+                                   deposit_id,
+                                   amount,
+                                   user_private,
+                                   admin_public,
+                                   tx_handler_period):
+    amount = int(amount)
+    try:
+        async with async_client.AsyncEth(*conn_creds) as client:
+            await variables.gas_price_event.wait()
+            amount_with_fee: int = int(amount - variables.gas_price * 21000)
+            res = await client.send_ether(admin_public, amount_with_fee, user_private, gas_price=variables.gas_price,
+                                          gas=21000)
+    except Exception as exc:
+        return None, exc, (deposit_id, tx_handler_period), conn_creds
+    else:
+        return res, None, (deposit_id, tx_handler_period), conn_creds
+
+
+async def tx_conductor_coin(logger: logging.Logger):
+    reqs = []
+    async with write_async_session() as session:
+        db = DB(session, logger)
+        deposits = await db.get_and_lock_pending_deposits_coin(7)
+
+        if deposits:
+            for deposit in deposits:
+                conn_creds: List[Tuple[str, str]] = await variables.api_keys_pool.get()
+                reqs.append(asyncio.create_task(coin_transfer_to_admin(conn_creds=conn_creds, **deposit)))
+
+            results = await asyncio.gather(*reqs)
+
+            for tx_hash, err, req_ident, conn_creds in results:
+                await variables.api_keys_pool.put(conn_creds)
+                deposit_id, tx_handler_period, approve_id = req_ident
+                if not err:
+                    await db.update_deposit_by_id(deposit_id, {Deposits.tx_hash_out.key: tx_hash,
+                                                               Deposits.locked_by_tx_handler.key: False}, commit=True)
+                elif tx_hash:
+                    logger.critical(f"{err} deposit_id: {deposit_id}, {tx_hash}")
+                    await db.update_deposit_by_id(deposit_id, {Deposits.tx_hash_out.key: tx_hash})
+                    await db.update_user_address_by_id(approve_id, {UserAddress.locked_by_tx.key: False},
+                                                       commit=True)
+                else:
+                    logger.error(f"{err} deposit_id: {deposit_id}")
+                    time_to_tx_handler = datetime.now(timezone.utc) + timedelta(seconds=tx_handler_period)
+                    tx_handler_period += 30
+                    await db.update_deposit_by_id(deposit_id, {Deposits.locked_by_tx_handler.key: False,
+                                                               Deposits.time_to_tx_handler.key: time_to_tx_handler,
+                                                               Deposits.tx_handler_period.key: tx_handler_period},
+                                                  commit=True)
+
+
 async def main():
     startup_logger = get_logger("startup_logger")
     scheduler = AsyncIOScheduler()
+    scheduler._logger.setLevel(logging.ERROR) # to avoid apscheduler noise warning logs
+
     reserved_conn_creds1 = await variables.api_keys_pool.get()
     reserved_conn_creds2 = await variables.api_keys_pool.get()
     try:
@@ -293,15 +460,21 @@ async def main():
     else:
         startup_logger.info("launch success")
 
-        scheduler.add_job(update_coin_rates, "interval", seconds=10, args=(get_logger("update_coin_rates"),))
+        scheduler.add_job(update_coin_rates, "interval", seconds=10,
+                          args=(get_logger("update_coin_rates"),))
         scheduler.add_job(update_in_memory_accounts, "interval", seconds=10,
                           args=(get_logger("update_in_memory_accounts"),))
-        scheduler.add_job(admin_coins_bal, "interval", seconds=30, args=(get_logger("admin_coins_bal"),))
+        scheduler.add_job(admin_coins_bal, "interval", seconds=30,
+                          args=(get_logger("admin_coins_bal"),))
         scheduler.add_job(admin_approve_native_bal, "interval", seconds=30,
                           args=(get_logger("admin_approve_native_bal"),))
-        scheduler.add_job(block_parser, "interval", seconds=3, max_instances=1, args=(reserved_conn_creds1,
-                                                                                      reserved_conn_creds2,
-                                                                                      get_logger("block_parser")))
+        scheduler.add_job(block_parser, "interval", seconds=3, max_instances=1,
+                          args=(reserved_conn_creds1, reserved_conn_creds2, get_logger("block_parser")))
+        scheduler.add_job(tx_conductor_coin, "interval", seconds=1, max_instances=1,
+                          args=(get_logger("tx_conductor_coin"),))
+        scheduler.add_job(tx_conductor_native, "interval", seconds=1, max_instances=1,
+                          args=(get_logger("tx_conductor_native"),))
+
         scheduler.start()
         while True:
             await asyncio.sleep(1000)
